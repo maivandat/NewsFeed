6d3b4948c4b64244dda23fca48967dca17333a4d
Van Dat <fanh.troll@gmail.com>
2019-04-15 21:28:22 +0700

    itent-activity news detail-finish app

--- app/src/main/java/vn/hueic/student/maivan/dat/newsfeed/ui/newsdetail/NewsDetailActivity.java
+++ app/src/main/java/vn/hueic/student/maivan/dat/newsfeed/ui/newsdetail/NewsDetailActivity.java
@@ -1,17 +1,24 @@
 package vn.hueic.student.maivan.dat.newsfeed.ui.newsdetail;
 
+import android.content.Intent;
 import android.os.BaseBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
+import vn.hueic.student.maivan.dat.newsfeed.AppContacts;
 import vn.hueic.student.maivan.dat.newsfeed.R;
 import vn.hueic.student.maivan.dat.newsfeed.ui.base.BaseActivity;
 
 public class NewsDetailActivity extends BaseActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_detail);
+        webView = findViewById(R.id.webview);
+        Intent intent = getIntent();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(intent.getStringExtra(AppContacts.KEY_LINK));
     }
 }
